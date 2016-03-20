029259152fa5e1349cf51519edf78d29322ac3e7
Zabrodin Konstantin <fragnatt@gmail.com>
2016-03-20 20:22:23 +0300

    наконец то удалось подключить роутер

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,27 @@
 import {Component} from 'angular2/core';
 import {UserListComponent} from './user/user-list.component';
+import {RouteConfig, ROUTER_DIRECTIVES} from "angular2/router";
+import {User} from "./user/User";
+import {EditorComponent} from "./editor/editor.component";
 
 @Component({
     selector: 'my-app',
     template: `
-    <user-list></user-list>
+       <h1>JaTumba</h1>
+      <nav>
+        <a [routerLink]="['UserList']">Коллектив</a>
+        <a [routerLink]="['Editor']">Редактор</a>
+      </nav>
+      <div>
+        <router-outlet></router-outlet>
+      </div>
     `,
-    directives: [UserListComponent]
+    directives: [ROUTER_DIRECTIVES]
 })
+@RouteConfig([
+    {path:'/user-list', name: 'UserList', component: UserListComponent},
+    {path:'/editor', name: 'Editor',       component: EditorComponent}
+])
 export class AppComponent {
     public headMenu = {
         editor : {
